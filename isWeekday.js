0650a6239072f6997646c8ff1692f646421ab404
MofokengTT21 <tseko9164@gmail.com>
2024-07-05 06:35:56 +0200

    format

--- isWeekday.js
+++ isWeekday.js
@@ -1,7 +1,7 @@
 function isWeekday(day) {
-    // Define an array of weekend days
-    var weekendDays = ['Saturday', 'Sunday'];
+  // Define an array of weekend days
+  var weekendDays = ["Saturday", "Sunday"];
 
-    // Check if the provided day is not in the weekendDays array
-    return !weekendDays.includes(day);
+  // Check if the provided day is not in the weekendDays array
+  return !weekendDays.includes(day);
 }
